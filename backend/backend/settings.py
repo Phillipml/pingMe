f2c5352753b96ce3cde373cff8182defed9030db
Phillipml <contato.phillip.menezes@gmail.com>
2025-10-28 10:40:04 -0300

    authentication config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -29,11 +29,19 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-  REST_FRAMEWORK = {
-      'DEFAULT_AUTHENTICATION_CLASSES': [
-          'rest_framework_simplejwt.authentication.JWTAuthentication',
-      ],
-  }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
+}
 
 INSTALLED_APPS = [
     "django.contrib.admin",
