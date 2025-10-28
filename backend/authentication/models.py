dcd93decef9b7816d9529ea62d92b0d1b855c3e5
Phillipml <contato.phillip.menezes@gmail.com>
2025-10-28 12:12:49 -0300

    migrations and models

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class User(AbstractUser):
     email = models.EmailField(unique=True)
     is_active = models.BooleanField(default=True)
-    create_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     groups = models.ManyToManyField(
