a7a0bbb12c66d821d8cb8ca44080aa620a5df74e
Phillipml <contato.phillip.menezes@gmail.com>
2025-10-30 13:44:53 -0300

    post data fix

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -40,7 +40,7 @@ class PostSerializer(serializers.ModelSerializer):
 class PostCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ["content", "image"]
+        fields = ["content"]
 
 
 class LikeSerializer(serializers.ModelSerializer):
