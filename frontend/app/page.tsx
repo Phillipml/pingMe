ea0641f96c4e4d0acb2ae4a4da51db813dc0a48d
Phillipml <contato.phillip.menezes@gmail.com>
2025-11-03 16:17:27 -0300

    login size fix

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,17 +2,17 @@ import { Logo } from "./components/ui/Logo";
 export default function Home() {
   return (
     <div className="w-full h-screen flex justify-center items-center ">
-      <div className="grid justify-center w-1/2 border-2 border-violet-800 text-center rounded p-4">
+      <div className="grid w-lg justify-center border-2 border-violet-800 text-center rounded p-4">
         <Logo className="text-2xl m-auto border-b-2 border-violet-600 pb-2 mb-8 mt-2" />
         <h2 className="mb-8">Login</h2>
         <input
           type="text"
-          className="border-2 border-violet-600 rounded p-2 w-lg mb-2"
+          className="border-2 border-violet-600 rounded p-2 mb-2 w-100"
           placeholder="Email"
         />
         <input
           type="text"
-          className="border-2 border-violet-600 rounded p-2 w-lg"
+          className="border-2 border-violet-600 rounded p-2 w-100"
           placeholder="Senha"
         />
         <button className="p-4 rounded text-center bg-violet-800 hover:bg-violet-600 cursor-pointer mt-4 mb-4 transition">
