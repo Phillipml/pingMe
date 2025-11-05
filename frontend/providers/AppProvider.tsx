93d7aaf1d1994ac11414e6ded79c3ba54ce13ef8
Phillipml <contato.phillip.menezes@gmail.com>
2025-11-05 15:23:57 -0300

    providers

---
+++ frontend/providers/AppProvider.tsx
@@ -0,0 +1,8 @@
+import { store } from "@/lib/store";
+import { Provider } from "react-redux";
+
+const AppProvider = ({ children }: { children: React.ReactNode }) => {
+  return <Provider store={store}>{children}</Provider>;
+};
+
+export default AppProvider;
