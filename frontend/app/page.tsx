199ff78726dce7d41018975fd0260a000dc0f8bd
Phillipml <contato.phillip.menezes@gmail.com>
2025-11-06 17:38:19 -0300

    fix AppPorvider and homePAge async function created

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <>HOme</>;
-}
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const cookieStore = await cookies()
+  const accessToken = cookieStore.get("accessToken")
+  const refreshToken = cookieStore.get("refreshToken")
+  if(!accessToken || !refreshToken) {
+     redirect("/login")
+  } else {
+    redirect("/feed")
+  }
+} 
