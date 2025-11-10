9e3300d1c35a01d1271d814c8b6f33c399c42d55
Phillipml <contato.phillip.menezes@gmail.com>
2025-11-10 10:05:25 -0300

    login screen workin great

--- frontend/hooks/useAuth.ts
+++ frontend/hooks/useAuth.ts
@@ -1,28 +1,15 @@
 "use client";
 
-import { useEffect, useState } from "react";
 import { useGetProfileQuery } from "@/lib/slice";
 
 export function useAuth() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
   const {
     data: user,
     isLoading: queryLoading,
     error,
   } = useGetProfileQuery();
 
-  useEffect(() => {
-    if (error) {
-      setIsAuthenticated(false);
-      return;
-    }
-
-    if (user) {
-      setIsAuthenticated(true);
-      return;
-    }
-  }, [user, error]);
+  const isAuthenticated = !error && !!user;
 
   return {
     isAuthenticated,
