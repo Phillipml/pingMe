f09933c291c83b8abfb1945c578b7ab9185303d2
Phillipml <contato.phillip.menezes@gmail.com>
2025-11-27 10:54:46 -0300

    complete-profile form and container

--- frontend/app/complete-profile/page.tsx
+++ frontend/app/complete-profile/page.tsx
@@ -12,32 +12,38 @@ export default function CompleteProfile() {
   const [lastname, setLastname] = useState('')
   const [bio, setBio] = useState('')
   return (
-    <CenterContainer>
-      <h2 className="text-center">Complete your Profile</h2>
-      <Logo className="text-2xl border-b-2 border-violet-600 pb-2 mb-8 mt-2" />
-      <Form>
-        <Input
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <Input
-          placeholder="Nome"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <Input
-          placeholder="Sobrenome"
-          value={lastname}
-          onChange={(e) => setLastname(e.target.value)}
-        />
-        <Input
-          placeholder="Bio"
-          value={bio}
-          onChange={(e) => setBio(e.target.value)}
-        />
-        <Button>Foto perfil</Button>
-      </Form>
+    <CenterContainer className="grid justify-center">
+      <div className="w-full max-w-md border-2 border-violet-600 rounded-md p-4">
+        <Logo className="text-2xl mx-auto justify-center border-b-2 border-violet-600 pb-2 mb-8 mt-2" />
+        <h2 className="text-center">
+          Hora de iniciar sua história aqui com a gente!
+          <br />
+          Vamos completar o seu perfil!
+        </h2>
+        <Form>
+          <Input
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <Input
+            placeholder="Nome"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Input
+            placeholder="Sobrenome"
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+          />
+          <Input
+            placeholder="Bio"
+            value={bio}
+            onChange={(e) => setBio(e.target.value)}
+          />
+          <Button>Foto perfil</Button>
+        </Form>
+      </div>
     </CenterContainer>
   )
 }
--- frontend/components/layout/Container.tsx
+++ frontend/components/layout/Container.tsx
@@ -1,7 +1,13 @@
 import React, { ReactNode } from 'react'
+type ContainerProps = {
+  children: React.ReactNode
+  className?: string
+}
 
-export default function Container({ children }: { children: ReactNode }) {
+export default function Container({ children, className }: ContainerProps) {
   return (
-    <div className="w-full max-w-[1400px] h-auto p-4 mx-auto">{children}</div>
+    <div className={`w-full max-w-[1400px] h-auto p-4 mx-auto ${className}`}>
+      {children}
+    </div>
   )
 }
