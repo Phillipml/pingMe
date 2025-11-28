216bc13216a7ff36ae5c647beae6a619d636e313
Phillipml <contato.phillip.menezes@gmail.com>
2025-11-28 10:13:24 -0300

    header elements and middleware for images

--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -2,18 +2,36 @@
 import { useGetProfileQuery } from '@/lib/slice'
 import Container from './Container'
 import { Logo } from '../ui/Logo'
+import { getMediaUrl } from '@/utils/api-utils'
+import Input from '../ui/Input'
+import { CiSearch } from 'react-icons/ci'
 
 export default function Header() {
   const { data, isLoading } = useGetProfileQuery()
   return (
     <header>
-      <Container className="flex justify-around bg-violet-600">
-        <Logo />
-        <div className="flex">
-          <img src={`${data?.info.avatar}`} className="pl-2 pr-2" />
+      <Container className="flex items-center bg-violet-600 p-2">
+        <div className="flex items-center justify-start flex-1">
+          <img
+            src={getMediaUrl(`${data?.info.avatar}`)}
+            className="rounded-full w-12 h-12 object-cover aspect-square flex-shrink-0"
+          />
           <h2 className="pl-2 pr-2">{isLoading ? '' : `${data?.username}`}</h2>
         </div>
-        <h2>Search</h2>
+        <div className="flex items-center justify-center flex-1">
+          <Logo />
+        </div>
+        <div className="flex items-center justify-end flex-1">
+          <div className="flex bg-gray-950 rounded-full p-0 w-64 mb-0 overflow-hidden">
+            <div className="flex items-center justify-center bg-gray-900 px-4 py-2">
+              <CiSearch className="text-gray-400" />
+            </div>
+            <Input
+              placeholder="Search"
+              className="border-none focus:outline-none bg-transparent flex-1"
+            />
+          </div>
+        </div>
       </Container>
     </header>
   )
--- frontend/utils/api-utils.ts
+++ frontend/utils/api-utils.ts
@@ -1,4 +1,11 @@
+import DefaultAvatar from '../public/user.png'
 export const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api'
 export const BACKEND_BASE_URL =
   process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:8000'
+
+export function getMediaUrl(path: string | null | undefined): string {
+  if (!path) return `${DefaultAvatar}`
+  if (path.startsWith('http')) return path
+  return `${BACKEND_BASE_URL}${path}`
+}
