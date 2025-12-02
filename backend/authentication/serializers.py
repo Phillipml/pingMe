060c1ad497cf1da5cf2a275c3a8d8423c4fd104b
Phillipml <contato.phillip.menezes@gmail.com>
2025-12-02 11:17:38 -0300

    username duplication fixed

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -15,6 +15,13 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         model = User
         fields = ["username", "email", "password"]
 
+    def validate_username(self, value):
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError(
+                "Este username já está em uso. Por favor, escolha outro."
+            )
+        return value
+
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         Profile.objects.create(user=user)
@@ -29,15 +36,44 @@ class ProfileSerializer(serializers.ModelSerializer):
         model = Profile
         fields = ["username", "first_name", "last_name", "bio", "avatar", "status"]
 
+    def validate_username(self, value):
+        if value:
+            if hasattr(self, "instance") and self.instance:
+                existing_user = (
+                    User.objects.filter(username=value)
+                    .exclude(id=self.instance.user.id)
+                    .first()
+                )
+            else:
+                existing_user = User.objects.filter(username=value).first()
+
+            if existing_user:
+                raise serializers.ValidationError(
+                    "Este username já está em uso. Por favor, escolha outro."
+                )
+        return value
+
     def update(self, instance, validated_data):
-        user_data = validated_data.pop('user', {})
+        user_data = validated_data.pop("user", {})
 
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
         instance.save()
-        if 'username' in user_data:
+
+        if "username" in user_data:
+            username = user_data["username"]
+            if (
+                User.objects.filter(username=username)
+                .exclude(id=instance.user.id)
+                .exists()
+            ):
+                raise serializers.ValidationError(
+                    {
+                        "username": "Este username já está em uso. Por favor, escolha outro."
+                    }
+                )
             user = instance.user
-            user.username = user_data['username']
+            user.username = username
             user.save()
 
         return instance
