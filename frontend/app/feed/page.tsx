027ed46814d937272411aa36c68506a3856c99b8
Phillipml <contato.phillip.menezes@gmail.com>
2025-12-05 15:39:07 -0300

    likes and comments button

--- frontend/app/feed/page.tsx
+++ frontend/app/feed/page.tsx
@@ -2,13 +2,16 @@
 import Container from '@/components/layout/Container'
 import Form from '@/components/layout/Form'
 import Button from '@/components/ui/Button'
-import { useCreatePostMutation } from '@/lib/slice'
+import { useCreatePostMutation, useFeedQuery } from '@/lib/slice'
 import { useState } from 'react'
+import { AiFillLike, AiOutlineLike } from 'react-icons/ai'
+import { MdOutlineInsertComment } from 'react-icons/md'
 
 export default function Feed() {
   const [post, setPost] = useState('')
   const [isPostCreated, setIsPostCreated] = useState(false)
   const [postData] = useCreatePostMutation()
+  const { data } = useFeedQuery()
   const createPost = async (e: React.FormEvent) => {
     e.preventDefault()
     try {
@@ -47,6 +50,39 @@ export default function Feed() {
             </Button>
           </div>
         </Form>
+      </div>
+      <div>
+        <ul className="flex flex-col justify-center items-center">
+          {data &&
+            data.results.map((post) => (
+              <li className="w-1/3  mt-4 grid pb-2">
+                <div className="flex justify-between ">
+                  <div className="flex items-center justify-center mb-4">
+                    <img
+                      src={post.author.avatar || ' '}
+                      alt=""
+                      className="w-8 h-8 object-cover rounded-full mr-2"
+                    />
+                    <h2>@{post.author.username}</h2>
+                  </div>
+                  <p className="text-gray-700">
+                    {new Date(post.author.created_at).toLocaleString('pt-BR')}
+                  </p>
+                </div>
+                <div className="border-b border-purple-950">{post.content}</div>
+                <div className="flex justify-around">
+                  <button className="flex items-center justify-center gap-1">
+                    <AiOutlineLike />
+                    {post.likes_count}
+                  </button>
+                  <button className="flex items-center justify-center gap-1">
+                    <MdOutlineInsertComment />
+                    {post.comments_count}
+                  </button>
+                </div>
+              </li>
+            ))}
+        </ul>
       </div>
     </Container>
   )
