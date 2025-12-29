c5ca081a42e2a62ee8447763364341a2df011289
Phillipml <contato.phillip.menezes@gmail.com>
2025-12-29 10:36:00 -0300

    ui responsive implemented

--- frontend/app/complete-profile/page.tsx
+++ frontend/app/complete-profile/page.tsx
@@ -33,7 +33,7 @@ export default function CompleteProfile() {
       setError(
         error?.data?.error || error.data?.message || 'Erro ao atualizar perfil'
       )
-    }
+    }3
   }
   return (
     <CenterContainer className="grid justify-center">
