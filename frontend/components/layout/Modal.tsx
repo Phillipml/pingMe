5a8ff6d5bc4fe096ddb2e6a72844bf2c439fd6c2
Phillipml <contato.phillip.menezes@gmail.com>
2025-12-30 22:43:54 -0300

    close button modal centered

--- frontend/components/layout/Modal.tsx
+++ frontend/components/layout/Modal.tsx
@@ -8,14 +8,16 @@ type Modal = {
 export default function Modal({ children, isOpen, onClose }: Modal) {
   if (!isOpen) return null
   return (
-    <div className="z-50 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 overflow-y-auto scrollbar-thin scrollbar-thumb-purple-700 scrollbar-track-gray-950 w-1/2 h-1/2 rounded bg-purple-950 border border-white">
+    <>
       <button
-        className="flex items-center justify-center absolute top-4 right-4 rounded-full w-8 h-8 cursor-pointer"
+        className="z-50 flex items-center justify-center fixed top-1/4 left-3/4 -translate-x-1/2 -translate-y-1/2 rounded-full w-8 h-8 cursor-pointer"
         onClick={onClose}
       >
-        <IoMdCloseCircleOutline className="w-full h-full bg-red-600 rounded-full" />
+        <IoMdCloseCircleOutline className="text-3xl bg-red-600 rounded-full" />
       </button>
-      {children}
-    </div>
+      <div className="z-40 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 overflow-y-auto scrollbar-thin scrollbar-thumb-purple-700 scrollbar-track-gray-950 w-1/2 h-1/2 rounded bg-purple-950 border border-white">
+        {children}
+      </div>
+    </>
   )
 }
